b326d693b4d7d6009efb6cfe8c93ba3ca7b449c5
Congduc Pham <Congduc.Pham@univ-pau.fr>
2022-06-04 16:00:50 +0200

    Update install.sh for DS3231 RTC support (need to uncomment)

--- Gateway/install.sh
+++ Gateway/install.sh
@@ -18,3 +18,13 @@ cd oled
 cd ..
 ./create_full_capacitive_device.sh
 
+##see https://www.raspberryme.com/ajout-dune-horloge-temps-reel-ds3231-au-raspberry-pi/
+
+#echo "adding support for additional DS3231 RTC module"
+#sudo apt install -y i2c-tools
+#cp /etc/modules ./tmp-etc-modules
+#echo "rtc-ds1307" >> ./tmp-modules
+#sudo cp ./tmp-etc-modules /etc/modules
+#rm ./tmp-etc-modules
+#sudo sed -i 's/^exit 0/echo ds1307 0x68 > \/sys\/class\/i2c-adapter\/i2c-1\/new_device\nhwclock -s\nexit 0/g' /etc/rc.local	
+
