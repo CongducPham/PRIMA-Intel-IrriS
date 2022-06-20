83bcf7bae84cae41701c77a8a885626f2b897ee2
Congduc Pham <Congduc.Pham@univ-pau.fr>
2022-06-20 13:00:37 +0200

    auto-config for AUA setting

---
+++ Gateway/boot/create-custom-example-AUA/intel-irris-auto-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+logger -t intel-irris-auto-config "create-custom-example-AUA"
+
+cd /home/pi/scripts
+#delete the default device
+./delete_sensor.sh
+#create capacitive SOIL-AREA-1 and device with address 26011DAA
+./create_full_capacitive_device.sh
+#create tensiometer SOIL-AREA-2 and device with address 26011DB2
+./create_full_2-tensiometer_device_with_dev_addr.sh 2 B2
+#create only the temperature sensor
+./create_only_temperature_sensor.sh 2
