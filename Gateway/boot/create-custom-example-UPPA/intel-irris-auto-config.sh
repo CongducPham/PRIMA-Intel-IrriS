34138e3ef46ef2bbbf2599d7d844f9939209e717
Congduc Pham <Congduc.Pham@univ-pau.fr>
2022-06-29 13:18:01 +0200

    Update auto-config scripts

--- Gateway/boot/create-custom-example-UPPA/intel-irris-auto-config.sh
+++ Gateway/boot/create-custom-example-UPPA/intel-irris-auto-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-logger -t intel-irris-auto-config "create-custom-example-AUA"
+logger -t intel-irris-auto-config "create-custom-example-UPPA"
 
 cd /home/pi/scripts
 #delete all sensors
@@ -8,7 +8,7 @@ cd /home/pi/scripts
 #create capacitive SOIL-AREA-1 and device with address 26011DAA
 ./create_full_capacitive_device.sh
 #create tensiometer SOIL-AREA-2 and device with address 26011DB2
-./create_full_2-tensiometer_device_with_dev_addr.sh 2 B2
+./create_full_tensiometer_device_with_dev_addr.sh 2 B2
 
 DEVICE=`cat /home/pi/scripts/LAST_CREATED_DEVICE.txt`
 #create only the temperature sensor
