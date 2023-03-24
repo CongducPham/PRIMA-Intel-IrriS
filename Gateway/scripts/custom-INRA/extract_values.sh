66a754af11c2017a09c28256e0b00f4b1c1aafeb
GuillaumeGaillard <8808589+Guillaumegaillard@users.noreply.github.com>
2023-03-24 15:26:59 +0100

    extract meta data

--- Gateway/scripts/custom-INRA/extract_values.sh
+++ Gateway/scripts/custom-INRA/extract_values.sh
@@ -4,16 +4,17 @@
 
 # empty previous file
 echo "" > extracted.json
+echo "" > extracted_meta.json
 
-echo "Get token"
+#echo "Get token"
 TOK=`curl -X POST "http://localhost/auth/token" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"username\":\"admin\",\"password\":\"loragateway\"}" | tr -d '\"'`
 
-echo "Get number of devices"
+#echo "Get number of devices"
 NDEVICE=`curl -X GET "http://localhost/devices" -H  "accept: application/json" | jq '. | length'`
 
 (( NDEVICE-- ))
 
-echo $NDEVICE
+#echo $NDEVICE
 
 #we leave device[0] which is usually the initial gateway declaration
 while [ $NDEVICE -gt 0 ]
@@ -22,12 +23,13 @@ do
   DEVICE=`curl -X GET "http://localhost/devices" -H  "accept: application/json" | jq ".[$NDEVICE].id" | tr -d '\"'`
   DEVNAME=`curl -X GET "http://localhost/devices" -H  "accept: application/json" | jq ".[$NDEVICE].name" | tr -d '\"'`
   DEVADDR=`curl -X GET "http://localhost/devices" -H  "accept: application/json" | jq ".[$NDEVICE].meta.lorawan.devAddr" | tr -d '\"'`
-  # DEVSENSORS=`curl -X GET "http://localhost/devices" -H  "accept: application/json" | jq ".[$NDEVICE].sensors" | tr -d '\"'`
-  # echo "${DEVSENSORS}"
+  DEVSENSORS=`curl -X GET "http://localhost/devices" -H  "accept: application/json" | jq ".[$NDEVICE].sensors"`
+  echo "${DEVNAME}:${DEVADDR}:${DEVICE}" >> extracted_meta.json
+  echo "${DEVSENSORS}" >> extracted_meta.json
   
   sizeDEVICE=${#DEVICE}
   
-  #we do not want to delete a gateway as it is also considered as a device
+  #we do not want to go through a gateway as it is also considered as a device
   if [ $sizeDEVICE -gt 12 ]
   then
     echo "${DEVNAME}:${DEVADDR}:${DEVICE}" >> extracted.json
