3f6bf8af377778f28e2da05e5893682563c671b2
Congduc Pham <Congduc.Pham@univ-pau.fr>
2023-06-20 12:48:05 +0200

    Update config_band.sh and IoT device tutorial

--- Gateway/scripts/config_band.sh
+++ Gateway/scripts/config_band.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "copy $1/global_conf.json to waziup.wazigate-lora.forwarders:/root/conf/single_chan_pkt_fwd/"
+echo "copy single_chan_pkt_fwd/$1/global_conf.json to waziup.wazigate-lora.forwarders:/root/conf/single_chan_pkt_fwd/"
 docker cp /home/pi/scripts/single_chan_pkt_fwd/$1/global_conf.json waziup.wazigate-lora.forwarders:/root/conf/single_chan_pkt_fwd/
 docker exec -it --user root waziup.wazigate-lora.forwarders chown root:root /root/conf/single_chan_pkt_fwd/global_conf.json
 
-echo "copy $1/global_conf.json to waziup.wazigate-lora.forwarders:/root/conf/multi_chan_pkt_fwd/"
+echo "copy multi_chan_pkt_fwd/$1/global_conf.json to waziup.wazigate-lora.forwarders:/root/conf/multi_chan_pkt_fwd/"
 docker cp /home/pi/scripts/multi_chan_pkt_fwd/$1/global_conf.json waziup.wazigate-lora.forwarders:/root/conf/multi_chan_pkt_fwd/
 docker exec -it --user root waziup.wazigate-lora.forwarders chown root:root /root/conf/multi_chan_pkt_fwd/global_conf.json
 
