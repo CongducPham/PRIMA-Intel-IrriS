92042f723f56f130c587fc3df863bbc3f380701a
GuillaumeGaillard <8808589+Guillaumegaillard@users.noreply.github.com>
2024-04-10 13:02:32 +0200

    indent preprocessor statements

--- Arduino/Intelirris_Soil_Sensor/BoardSettings.h
+++ Arduino/Intelirris_Soil_Sensor/BoardSettings.h
@@ -1,53 +1,53 @@
 #ifndef BOARDSETTINGS_H
-#define BOARDSETTINGS_H
+  #define BOARDSETTINGS_H
 
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
+  #if ARDUINO >= 100
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+  #endif
 
-////////////////////////////////////////////////////////////////////
-//uncomment for WAZISENSE v2 board
-//#define WAZISENSE
+  ////////////////////////////////////////////////////////////////////
+  // uncomment for WAZISENSE v2 board
+  // #define WAZISENSE
 
-////////////////////////////////////////////////////////////////////
-//uncomment for IRD PCB board
-//#define IRD_PCB
-//also uncomment for IRD PCB board fully assembled by manufacturer
-//with all components, including solar circuit
-//#define IRD_PCBA
+  ////////////////////////////////////////////////////////////////////
+  // uncomment for IRD PCB board
+  // #define IRD_PCB
+  // also uncomment for IRD PCB board fully assembled by manufacturer
+  // with all components, including solar circuit
+  // #define IRD_PCBA
 
-////////////////////////////////////////////////////////////////////
-// uncomment only if the IRD PCB or PCBA is running on solar panel
-// MUST be commented if running on alkaline battery
-// code for SOLAR_BAT has been written by Jean-François Printanier from IRD
-//#define SOLAR_BAT
-// do not change if you are not knowing what you are doing
-#define NIMH
+  ////////////////////////////////////////////////////////////////////
+  // uncomment only if the IRD PCB or PCBA is running on solar panel
+  // MUST be commented if running on alkaline battery
+  // code for SOLAR_BAT has been written by Jean-François Printanier from IRD
+  // #define SOLAR_BAT
+  // do not change if you are not knowing what you are doing
+  #define NIMH
 
-// For powering humdity sensor and DS18B20
-#if (defined IRD_PCB && defined SOLAR_BAT) || defined IRD_PCBA
+  // For powering humdity sensor and DS18B20
+  #if (defined IRD_PCB && defined SOLAR_BAT) || defined IRD_PCBA
 
-#define PWR_SOFT_START_LOW   20
-#define PWR_SOFT_START_HIGH  20
+    #define PWR_SOFT_START_LOW   20
+    #define PWR_SOFT_START_HIGH  20
 
-#define PWR_LOW     HIGH  // mosfet P
-#define PWR_HIGH    LOW   // mosfet P
+    #define PWR_LOW     HIGH  // mosfet P
+    #define PWR_HIGH    LOW   // mosfet P
 
-void power_soft_start( uint8_t pin);
+    void power_soft_start( uint8_t pin);
 
-#else
+  #else
 
-#define PWR_LOW     LOW   // gpio
-#define PWR_HIGH    HIGH  // gpio
+    #define PWR_LOW     LOW   // gpio
+    #define PWR_HIGH    HIGH  // gpio
 
-#endif
+  #endif
 
-#ifdef IRD_PCBA
-#ifndef IRD_PCB
-#define IRD_PCB
-#endif
-#endif
+  #ifdef IRD_PCBA
+    #ifndef IRD_PCB
+      #define IRD_PCB
+    #endif
+  #endif
 
 #endif // BOARDSETTINGS_H
